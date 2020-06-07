50c8b87c92d24f5ae6d04c94310e76aff2599a46
Francesca Hu <francesca.hu@hotmail.it>
2020-06-08 01:57:42 +0200

    SinkThem

--- src/main/java/m2ex/SinkThem.java
+++ src/main/java/m2ex/SinkThem.java
@@ -1,6 +1,8 @@
 package m2ex;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class SinkThem {
     static public final char SHIP = 'S';
@@ -26,7 +28,13 @@ public class SinkThem {
      */
     public SinkThem(int dimension) {
         // TODO
-        board = new char[0][0];
+        board = new char[dimension][dimension]; //dimension
+        
+        for(int i = 0; i < board.length; i++) {
+        	for(int j = 0; j < board.length; j++) {
+        		board[i][j] = EMPTY;
+        	}
+        }
     }
 
     /**
@@ -63,19 +71,47 @@ public class SinkThem {
      */
     public String getBoard() {
         // TODO
-        return "* * *\n* * *\n* * *\n";
+    	StringBuilder stampa = new StringBuilder();
+    	for(int i = 0; i < getBoardSize(); i++) {
+    		
+    		for(int j = 0; j < getBoardSize(); j++) {
+    			
+    			if(board[i][j] == MISS || board[i][j] == WRECK) {
+    				stampa.append(board[i][j] + "\t");
+    			}else if(board[i][j] == EMPTY || board[i][j] == SHIP) {
+    				stampa.append(UNKNOWN + "\t");
+    			}else{
+    				System.out.print("ELSE");
+    			}
+    		}
+    		stampa.append("\n");
+    	}
+    	
+    	System.out.print(stampa.toString());
+    	
+        return stampa.toString() + "\n\n* * *\n* * *\n* * *\n\n";
     }
 
     /**
      * Place a ship on the board
+     * increment counter if placing succeeded.
      * 
      * @param row
      * @param col
      * @return false if it can't be placed
      */
     public boolean place(int row, int col) {
-        // TODO
-        return false;
+    	if(row < 0 || row > board.length || col < 0 || col > board.length)
+    		throw new IllegalArgumentException("Place input parameters");
+    	
+    	boolean res = false;
+    	
+        if(board[row][col] == EMPTY) {
+        	board[row][col] = SHIP;
+        	counter++;
+        	res = true;
+        }
+        return res;
     }
 
     /**
@@ -88,8 +124,26 @@ public class SinkThem {
      * @return true for a sink
      */
     public boolean shoot(int row, int col) {
-        // TODO
-        return false;
+    	// TODO
+    	if(row < 0 || row > board.length || col < 0 || col > board.length)
+    		throw new IllegalArgumentException("Place input parameters");
+        
+    	boolean res = false;
+    	
+    	if(board[row][col] == SHIP) {
+    		board[row][col] = WRECK;
+    		points += POINTS_FOR_SINK;
+    		counter--;
+    		System.out.println("WRECK!");
+    		res = true;
+    	}else if(board[row][col] == EMPTY) {
+    		board[row][col] = MISS;
+    		points -= POINTS_FOR_MISS;
+    		System.out.println("MISS!");
+    		res = false;
+    	}
+    		
+        return res;
     }
 
     @Override
@@ -121,18 +175,51 @@ public class SinkThem {
 
     public static void main(String[] args) {
         // TODO: use Scanner for user interaction
-
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("Insert board dimension: ");
+    	int dim = 0;
+    	if(scanner.hasNextInt()) {
+    		dim = scanner.nextInt();
+    	}
         // TODO: let the player choose for a (sensible) board size
-        SinkThem st = new SinkThem(10);
+        SinkThem st = new SinkThem(dim);
 
         // TODO: place the ships randomly instead
-        st.place(0, 2);
-        st.place(1, 1);
-        st.place(2, 0);
-
+//        st.place(0, 2);
+//        st.place(1, 1);
+//        st.place(2, 0);
+        
+        System.out.println("How many ships do you want to place? ");
+        int num = 0;
+    	if(scanner.hasNextInt()) {
+    		num = scanner.nextInt();
+    	}
+    	
+    	Random r = new Random();
+        for(int i = 0; i < num; i++) {
+        	st.place(r.nextInt(dim), r.nextInt(dim));
+        }
+ 
         // TODO: use Scanner instead
-        shootAll(st);
-
+//        shootAll(st);
+        while(!st.done()) {
+        	System.out.println("Insert x: ");
+        	
+        	int x = -1, y = -1;
+        	if(scanner.hasNextInt()) {
+        		x = scanner.nextInt();
+        	}
+        	
+        	System.out.println("Insert y: ");
+        	if(scanner.hasNextInt()) {
+        		y = scanner.nextInt();
+        	}
+        	
+        	st.shoot(x, y);
+        }
+        
+        scanner.close();
+       
         System.out.println(st);
         System.out.println("You scored " + st.getPoints());
     }
